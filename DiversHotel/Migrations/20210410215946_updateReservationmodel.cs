8eac8ac2d977cc26a74d43bd405efd6cceb46fae
KhALeD SaBrY <khaledsab1997@gmail.com>
2021-04-11 02:07:45 +0200

    adding reservation migrations

---
+++ DiversHotel/Migrations/20210410215946_updateReservationmodel.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DiversHotel.Migrations
+{
+    public partial class updateReservationmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Rooms_RoomId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_RoomId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Reservations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomType",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Guests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ReservationRoom",
+                columns: table => new
+                {
+                    ReservationsId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoomsId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservationRoom", x => new { x.ReservationsId, x.RoomsId });
+                    table.ForeignKey(
+                        name: "FK_ReservationRoom_Reservations_ReservationsId",
+                        column: x => x.ReservationsId,
+                        principalTable: "Reservations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReservationRoom_Rooms_RoomsId",
+                        column: x => x.RoomsId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationRoom_RoomsId",
+                table: "ReservationRoom",
+                column: "RoomsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReservationRoom");
+
+            migrationBuilder.DropColumn(
+                name: "RoomType",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Guests");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomId",
+                table: "Reservations",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_RoomId",
+                table: "Reservations",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Rooms_RoomId",
+                table: "Reservations",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
